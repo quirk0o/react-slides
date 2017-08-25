e5548349c319a51d95bf8cd1ea752c6ed9dabe88
Beata Obrok <beata.obrok@u2i.com>
2017-08-25 12:41:28 +0200

    [Refactor] slide routing creation

--- src/app.jsx
+++ src/app.jsx
@@ -2,218 +2,135 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 
 import {Layout} from 'modules/layout'
-import {CMEditor} from 'modules/editor'
 import {Slide} from 'modules/slides'
+
+const jsSlides = [
+  {
+    header: 'Function scope',
+    description: '',
+    code: require('!raw-loader!./examples/js/function')
+  },
+  {
+    header: 'Closures',
+    description: '',
+    code: require('!raw-loader!./examples/js/closures')
+  },
+  {
+    header: 'Bind',
+    description: '',
+    code: require('!raw-loader!./examples/js/bind')
+  },
+  {
+    header: 'Let, Const',
+    description: '',
+    code: require('!raw-loader!./examples/js/letconst')
+  },
+  {
+    header: 'Arrow functions',
+    description: '',
+    code: require('!raw-loader!./examples/js/arrow')
+  },
+  {
+    header: 'String interpolation',
+    description: '',
+    code: require('!raw-loader!./examples/js/interpolation')
+  },
+  {
+    header: 'Object destructuring',
+    description: '',
+    code: require('!raw-loader!./examples/js/destructuring')
+  },
+  {
+    header: 'Default function arguments',
+    description: '',
+    code: require('!raw-loader!./examples/js/default-arguments')
+  },
+  {
+    header: 'Spread operator',
+    description: '',
+    code: require('!raw-loader!./examples/js/spread')
+  },
+  {
+    header: 'Classes',
+    description: '',
+    code: require('!raw-loader!./examples/js/class')
+  }
+]
+
+const reactSlides = [
+  {
+    header: 'Stateless Component',
+    description: '',
+    code: require('!raw-loader!./examples/react/stateless')
+  },
+  {
+    header: 'Stateful Component',
+    description: '',
+    code: require('!raw-loader!./examples/react/stateful')
+  },
+  {
+    header: 'Uncontrolled input',
+    description: '',
+    code: require('!raw-loader!./examples/react/uncontrolled')
+  },
+  {
+    header: 'Lifecycle Methods Example',
+    description: '',
+    code: require('!raw-loader!./examples/react/lifecycle')
+  },
+  {
+    header: 'Lifecycle Methods Continued',
+    description: '',
+    code: require('!raw-loader!./examples/react/lifecycle-cd')
+  },
+  {
+    header: 'Prop Types',
+    description: '',
+    code: require('!raw-loader!./examples/react/proptypes')
+  },
+  {
+    header: 'Children',
+    description: '',
+    code: require('!raw-loader!./examples/react/children')
+  },
+  {
+    header: 'Styles',
+    description: '',
+    code: require('!raw-loader!./examples/react/styles')
+  },
+  {}
+]
 
 const App = () => (
   <Layout>
-    <Route
-      exact
-      path="/js/1"
-      render={() =>
-        <Slide
-          id={1}
-          maxId={9}
-          header="Function scope, closures"
-          description=""
-          code={require('!raw-loader!./examples/js/function')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/2"
-      render={() =>
-        <Slide
-          id={2}
-          maxId={9}
-          header="Bind"
-          description=""
-          code={require('!raw-loader!./examples/js/bind')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/3"
-      render={() =>
-        <Slide
-          id={3}
-          maxId={9}
-          header="Let, Const"
-          description=""
-          code={require('!raw-loader!./examples/js/letconst')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/4"
-      render={() =>
-        <Slide
-          id={4}
-          maxId={9}
-          header="Arrow functions"
-          description=""
-          code={require('!raw-loader!./examples/js/arrow')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/5"
-      render={() =>
-        <Slide
-          id={5}
-          maxId={9}
-          header="String interpolation"
-          description=""
-          code={require('!raw-loader!./examples/js/interpolation')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/6"
-      render={() =>
-        <Slide
-          id={6}
-          maxId={9}
-          header="Object destructuring"
-          description=""
-          code={require('!raw-loader!./examples/js/destructuring')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/7"
-      render={() =>
-        <Slide
-          id={7}
-          maxId={9}
-          header="Default function arguments"
-          description=""
-          code={require('!raw-loader!./examples/js/default-arguments')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/8"
-      render={() =>
-        <Slide
-          id={8}
-          maxId={9}
-          header="Spread operator"
-          description=""
-          code={require('!raw-loader!./examples/js/spread')}
-        />}
-    />
-    <Route
-      exact
-      path="/js/9"
-      render={() =>
-        <Slide
-          id={9}
-          maxId={9}
-          header="Classes"
-          description=""
-          code={require('!raw-loader!./examples/js/class')}
-        />}
-    />
-
-    <Route
-      exact
-      path="/react/1"
-      render={() =>
-        <Slide
-          id={1}
-          maxId={8}
-          header="Stateless Component"
-          description=""
-          code={require('!raw-loader!./examples/react/stateless')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/2"
-      render={() =>
-        <Slide
-          id={2}
-          maxId={8}
-          header="Stateful Component"
-          description=""
-          code={require('!raw-loader!./examples/react/stateful')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/3"
-      render={() =>
-        <Slide
-          id={3}
-          maxId={8}
-          header="Uncontrolled input"
-          description=""
-          code={require('!raw-loader!./examples/react/uncontrolled')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/4"
-      render={() =>
-        <Slide
-          id={4}
-          maxId={8}
-          header="Lifecycle Methods Example"
-          description=""
-          code={require('!raw-loader!./examples/react/lifecycle')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/5"
-      render={() =>
-        <Slide
-          id={5}
-          maxId={8}
-          header="Lifecycle Methods Continued"
-          description=""
-          code={require('!raw-loader!./examples/react/lifecycle-cd')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/6"
-      render={() =>
-        <Slide
-          id={6}
-          maxId={8}
-          header="Prop Types"
-          description=""
-          code={require('!raw-loader!./examples/react/proptypes')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/7"
-      render={() =>
-        <Slide
-          id={7}
-          maxId={8}
-          header="Children"
-          description=""
-          code={require('!raw-loader!./examples/react/children')}
-        />}
-    />
-    <Route
-      exact
-      path="/react/8"
-      render={() =>
-        <Slide
-          id={8}
-          maxId={8}
-          header="Styles"
-          description=""
-          code={require('!raw-loader!./examples/react/styles')}
-        />}
-    />
-
-
+    {jsSlides.map((slide, idx) => (
+      <Route
+        key={`js_${idx}`}
+        exact
+        path={`/js/${idx + 1}`}
+        render={() => (
+          <Slide
+            id={idx + 1}
+            maxId={jsSlides.length}
+            {...slide}
+          />
+        )}
+      />
+    ))}
+    {reactSlides.map((slide, idx) => (
+      <Route
+        key={`react_${idx}`}
+        exact
+        path={`/react/${idx + 1}`}
+        render={() => (
+          <Slide
+            id={idx + 1}
+            maxId={reactSlides.length}
+            {...slide}
+          />
+        )}
+      />
+    ))}
   </Layout>
 )
 
